ef13dc5784d2607bc311c199db92a88abcd67b06
Tensai99 <54536548+Tensai99@users.noreply.github.com>
2024-02-08 16:13:40 +0200

    Add files via upload

---
+++ run.py
@@ -0,0 +1,20 @@
+from app import app, db
+from app.models import User
+
+# Create the application context
+with app.app_context():
+    # Create all database tables
+    db.create_all()
+
+    # Check if the admin user already exists
+    admin_user = User.query.filter_by(username='admin').first()
+    if not admin_user:
+        # Create the admin user
+        admin_user = User(username='admin', email='eimtingwi@gmail.com', is_admin=True)
+        admin_user.set_password('vanawsum')
+        db.session.add(admin_user)
+        db.session.commit()
+
+# Run the Flask application
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
