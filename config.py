f1634a397507ce6cc4c79c6ab989cef1bbe57cef
Tensai99 <54536548+Tensai99@users.noreply.github.com>
2024-02-17 10:18:31 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ class Config:
     SECRET_KEY = os.urandom(24)
     SQLALCHEMY_DATABASE_URI ='mysql://root:root@localhost/restaurant'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    POSTMAN_MAILER_API_KEY = "PMAK-65cb6a918a25cc0001fb39ec-3fd8966aa5b3debfe243b74991582008d9"
+    POSTMAN_MAILER_API_KEY = ""
